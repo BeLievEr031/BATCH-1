6cb3b8b57620488f40a8d8f4593c0864692a89a6
BeLieVer031 <sandyrajak031@gmail.com>
2023-07-13 22:56:08 +0530

    section-one created

---
+++ 12-07-23/script.js
@@ -0,0 +1,39 @@
+// int variableName = 45;
+// string variableName = 45;
+// bool variableName = 45;
+// float variableName = 45;
+
+
+// var, let, const
+
+// var age = 20;
+// let age = 25;
+
+// let age = "Twenty five";
+// age = 30;
+// const email = "sandy@gmail.com"
+// email = "rajesh@gmail.com"
+// console.log(email);
+
+// let sum = 3+3;
+// console.log(sum);
+
+// != Not equals
+
+// let name = "sachin"
+
+// if (name == "sachin") {
+
+//     console.log("Hey sandeep");
+
+// } 
+// else if (name == "sandy") {
+
+//     console.log("Hey sachin");
+// }
+
+// else {
+//     console.log("Hey someone");
+// }
+
+let dayNo = 0;
