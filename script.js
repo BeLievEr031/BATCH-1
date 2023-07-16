41a7624ee50fe7ff61e434d2210629125dcd18f4
BeLieVer031 <sandyrajak031@gmail.com>
2023-07-16 20:38:20 +0530

    upto product card

---
+++ script.js
@@ -0,0 +1,35 @@
+// console.log(31);
+
+// let a = 45;
+// let b = 45;
+
+
+// let str = "hello world"
+
+// 0,  1    2        3  4
+let arr = [1, 2, 3, 45, 90];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+arr[0] = 1000;
+
+
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+}
+
+let obj = {
+    name: "sandeep",
+    age: 20,
+    email: "sandy@gmail.com",
+    email: "sandyrajak@gmail.com",
+}
+
+console.log(obj.age);
+
+obj.age = 1325;
+
+console.log(obj.age);
